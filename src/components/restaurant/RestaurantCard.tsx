925c033709da4a939ccaeb63f98871aedf1bbf66
Codergoterrors <c0d3r.x0@gmail.com>
2026-05-22 12:55:47 +0530

    fix(customer): fix absoluteFill type reference in RestaurantCard component

--- src/components/restaurant/RestaurantCard.tsx
+++ src/components/restaurant/RestaurantCard.tsx
@@ -123,7 +123,7 @@ const styles = StyleSheet.create({
     height: 160,
   },
   closedOverlay: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     backgroundColor: 'rgba(0,0,0,0.55)',
     justifyContent: 'center',
     alignItems: 'center',
