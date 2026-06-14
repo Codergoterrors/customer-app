43b124a51a915c26631efe42d6b16e8e94448101
Codergoterrors <c0d3r.x0@gmail.com>
2026-06-14 14:30:00 +0530

    feat: add useInterval hook for cleaner polling

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * A custom hook for setting up intervals that survive re-renders.
+ */
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef<() => void>(null);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay !== null) {
+      const id = setInterval(() => savedCallback.current?.(), delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
